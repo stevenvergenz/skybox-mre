31dbedc9f0d8d63fe2ac019f781eace8a8144795
Steven Vergenz <vergenzs@gmail.com>
2020-08-15 16:26:22 -0700

    Start infra for MRE

--- skybox-stellarium/start.sh
+++ skybox-stellarium/start.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+
+# start X
+rm /tmp/.X99-lock
 Xvfb :99 -screen 0 1024x1024x16 -nolisten tcp &
-DISPLAY=:99 /usr/bin/stellarium -c ./config.ini
+
+# clear screenshot dir
+rm -f /root/screenshots/*
+
+# start stellarium
+DISPLAY=:99 /usr/bin/stellarium -c ./config.ini --screenshot-dir /root/screenshots
