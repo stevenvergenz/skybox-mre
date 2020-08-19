65c1c4ceedf2cb3a465f560b6d770f179ccab512
Steven Vergenz <vergenzs@gmail.com>
2020-08-18 22:35:56 -0700

    It works!

--- skybox-mre/src/app.ts
+++ skybox-mre/src/app.ts
@@ -1,6 +1,9 @@
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 
+import * as Stellarium from './stellarium';
+
 export default class App {
+	private assets: MRE.AssetContainer;
 	private skyboxAssets: MRE.AssetContainer;
 
 	public constructor(public context: MRE.Context, public params: MRE.ParameterSet) {
@@ -8,8 +11,29 @@ export default class App {
 	}
 
 	private async start() {
+		this.assets = new MRE.AssetContainer(this.context);
+		const cube = await this.assets.loadGltf('cubemap.glb');
+
 		this.skyboxAssets = new MRE.AssetContainer(this.context);
-		const cube = await this.skyboxAssets.loadGltf('cubemap.glb');
+		const skybox = await Stellarium.takeSkybox(
+			{ latitude: 47.60621, longitude: -122.33207 },
+			new Date(),
+			this.context.sessionId);
+
+		for (const mat of this.assets.materials) {
+			if (mat.name === 'north') {
+				mat.emissiveTexture = this.skyboxAssets.createTexture('north', { uri: skybox.north } );
+			} else if (mat.name === 'south') {
+				mat.emissiveTexture = this.skyboxAssets.createTexture('south', { uri: skybox.south } );
+			} else if (mat.name === 'east') {
+				mat.emissiveTexture = this.skyboxAssets.createTexture('east', { uri: skybox.east } );
+			} else if (mat.name === 'west') {
+				mat.emissiveTexture = this.skyboxAssets.createTexture('west', { uri: skybox.west } );
+			} else if (mat.name === 'up') {
+				mat.emissiveTexture = this.skyboxAssets.createTexture('up', { uri: skybox.up } );
+			}
+		}
+
 		MRE.Actor.CreateFromPrefab(this.context, {
 			firstPrefabFrom: cube,
 			actor: {
--- skybox-mre/src/index.ts
+++ skybox-mre/src/index.ts
@@ -3,24 +3,17 @@ import * as Restify from 'restify';
 import { resolve } from 'path';
 
 import App from './app';
-import * as Stellarium from './stellarium';
 
 process.on('uncaughtException', err => console.error(err));
 process.on('unhandledRejection', err => console.error(err));
-
-Stellarium.takeSkybox(
-	{ latitude: 47.60621, longitude: -122.33207 },
-	new Date(),
-	"skybox"
-);
 
 const server = new MRE.WebHost({
 	baseDir: resolve(__dirname, "../public"),
 	optionalPermissions: [MRE.Permissions.UserInteraction]
 });
 
-/*setTimeout(() => {
+setTimeout(() => {
 	server.adapter.server.get('/skies/*', Restify.plugins.serveStaticFiles(process.env.STEL_OUTDIR));
-}, 1000);*/
+}, 1000);
 
 server.adapter.onConnection((context, params) => new App(context, params));
